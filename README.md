18dd28850cc07f128b6a002e6cddfd4f131efbd4
notechallemotion <shiwr313@gmail.com>
2022-03-19 14:58:55 +0800

    cookie setting instruction

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # BUTTERFLY
 
 ## 依赖安装
+
 `pip install -r setup.config`
 
-python版本：3.9.1
+python 版本：3.9.1
 
 ## 启动
+
 `python main.py`
 
-## TO-DO 
+启动前，需要设置`configs.py`中的`COOKIE`为自己登陆豆瓣后的浏览器 cookie。
+
+## TO-DO
+
 - [ ] 切换 host 功能
 - [ ] 主贴内容解析排版
 - [ ] 帖子回复中的引用内容
--- configs.py
+++ configs.py
@@ -2,6 +2,6 @@
 
 REPLY_COUNT_LOWER_LIMIT = 50
 
-COOKIE = 'bid=BOUOFPnYtvw; ll="108258"; _pk_ses.100001.8cb4=*; __utma=30149280.832003808.1647588157.1647588157.1647588157.1; __utmc=30149280; __utmz=30149280.1647588157.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; dbcl2="246566400:xsHAA2pgpDg"; ck=Y0aB; ucf_uid=175c31f1-26d0-4bb4-bba2-4850b9811258; _pk_id.100001.8cb4=24df1f8b68ea0fca.1647588149.1.1647588402.1647588149.; __gads=ID=bf907342d5c92f28:T=1647588398:S=ALNI_MYEDA25NrI4c6ZiPFSSKKxnmjFpTg; push_noty_num=0; push_doumail_num=0; __utmv=30149280.24656; __utmb=30149280.6.9.1647588417730'
+COOKIE = ''
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36'
 
